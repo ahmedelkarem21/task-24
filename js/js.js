4e4a43322a14667ce61ee580f407be2cbe55fc5f
ahmed_elkarem <ahmed_elkarem888@yahoo.com>
2024-02-24 18:14:28 +0200

    this is first push

---
+++ js/js.js
@@ -0,0 +1,43 @@
+// function person (a , b) {
+//     return a + (2023-b)
+// }
+
+// var username = prompt('enter your name')
+//     age = +prompt('enter your birth date')
+
+// console.log('your name is ' + username + ' and your age is ' +  age*365);    
+
+
+
+// var username = prompt('Enter your name'),
+//     userage = +prompt('enter your age');
+
+// function age (x) {
+//     return (2023 - X) * 365
+// }    
+
+
+// var clientName = prompt("Please enter your name"),
+//     clientAge = +prompt("Please enter your year of birth"),
+//     currentYear = 2023
+
+//     displayClientData(clientName, clientAge)
+
+// function getAge(x) {
+//     return (currentYear - x)
+// }
+
+// function displayClientData(x,y) {
+//     console.log("Hello " + x + " Your age is " + (2023-y) + " and your age in days is " + (2023-y) * 365)
+// }
+
+var username = prompt('Enter your name'),
+    dateofbirth = +prompt('Enter your date of birth')
+
+
+ function personData(x ,y) {
+    console.log('hello ' + x + ' your age is ' + (2023-y) + ' and your age in days is ' + (2023-y)*360)
+ }  
+ 
+    personData(username, dateofbirth)
+
